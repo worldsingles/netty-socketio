422ddf267481442e143927e2e6bbc9f62e532cf7
Kevin Downey <kevin@worldsingles.com>
2022-08-03 17:07:46 +0000

    count utf8 character bytes via abstract machine

--- src/main/java/com/corundumstudio/socketio/protocol/UTF8CharsScanner.java
+++ src/main/java/com/corundumstudio/socketio/protocol/UTF8CharsScanner.java
@@ -86,17 +86,126 @@ public class UTF8CharsScanner {
         return i;
     }
 
-    public int getActualLength(ByteBuf inputBuffer, int length) {
+    public int getLength(ByteBuf inputBuffer, int start) {
         int len = 0;
-        int start = inputBuffer.readerIndex();
-        for (int i = inputBuffer.readerIndex(); i < inputBuffer.readableBytes() + inputBuffer.readerIndex();) {
+        for (int i = start; i < inputBuffer.writerIndex();) {
             i = getCharTailIndex(inputBuffer, i);
             len++;
-            if (length == len) {
-                return i-start;
+        }
+        return len;
+    }
+
+    // public int getActualLength(ByteBuf inputBuffer, int length) {
+    //     int len = 0;
+    //     int start = inputBuffer.readerIndex();
+    //     for (int i = inputBuffer.readerIndex(); i < inputBuffer.readableBytes() + inputBuffer.readerIndex();) {
+    //         i = getCharTailIndex(inputBuffer, i);
+    //         len++;
+    //         if (length == len) {
+    //             return i-start;
+    //         }
+    //     }
+    //     throw new IllegalStateException();
+    // }
+
+    public enum Op {
+        READ,
+        DISPATCH,
+        COUNT1,
+        COUNT2,
+        COUNT3,
+        COUNT4,
+        END
+    }
+    
+    public int getActualLength(ByteBuf inputBuffer, int characterCount) {
+        // Uses an abstract machine to scan inputBuffer to find the
+        // byte length of characterCount characters.
+
+        // The registers of the machine are:
+        // stack: a 4 byte stack of bytes read from inputBuffer
+        // control: a stack of operations to perform
+        // i: the current byte index into inputBuffer for reading
+        // c: the number of characters left to count
+
+        // The operations of the machine are:
+        // READ: reads a byte and pushes it on to stack
+        // DISPATCH: examines the byte on the top of stack to determine how large this character is
+        // COUNT1: handles a 1 byte character
+        // COUNT2: handles a 2 byte character
+        // COUNT3: handles a 3 byte character
+        // COUNT4: handles a 4 byte character (which is two characters)
+        // END: the end of the control stack, if there are more characters to count, does that, otherwise exits
+
+        int stack = 0;
+        // top most stack element for convience
+        byte b = 0;
+        java.util.LinkedList<Op> control = new java.util.LinkedList();
+        control.addFirst(Op.END);
+        int i = 0;
+        int c = characterCount;
+        
+        while(true) {
+            switch(control.remove()) {
+            case END:
+                if(c > 0){
+                    control.addFirst(Op.END);
+                    control.addFirst(Op.DISPATCH);
+                    control.addFirst(Op.READ);
+                } else if (c == 0) {
+                    // i is the next byte after the last
+                    return i - 1;
+                } else {
+                    throw new IllegalStateException("Too many characters");
+                }
+                break;
+            case READ:
+                if((inputBuffer.readerIndex() + 1) > inputBuffer.readableBytes())
+                    throw new IllegalStateException("Not enough bytes");
+                // acts like reading a byte and pushing it on to the stack
+                b=inputBuffer.getByte(inputBuffer.readerIndex() + i);
+                stack=(stack << 8) | b;
+                break;
+            case DISPATCH:
+                // looks at the byte on the top of the stack (b0) and
+                // determines how many bytes are in the character
+                // based on the number of high bits
+                // https://en.m.wikipedia.org/wiki/UTF-8#Description
+                if( 0 == (b & (byte)-128)) {
+                    control.addFirst(Op.COUNT1);
+                } else if(-16 == (b & (byte) -16)) {
+                    control.addFirst(Op.COUNT4);
+                    control.addFirst(Op.READ);
+                    control.addFirst(Op.READ);
+                    control.addFirst(Op.READ);
+                } else if(-32 == (b & (byte) -32)) {
+                    control.addFirst(Op.COUNT3);
+                    control.addFirst(Op.READ);
+                    control.addFirst(Op.READ);
+                } else if(-64 == (b & (byte)-64)) {
+                    control.addFirst(Op.COUNT2);
+                    control.addFirst(Op.READ);
+                }
+                break;
+            case COUNT1:
+                i++;
+                c=c-java.lang.Character.charCount((int)b);
+                break;
+            case COUNT2:
+                i=i+2;
+                c=c-java.lang.Character.charCount(stack & 0xffff);
+                break;
+            case COUNT3:
+                i=i+3;
+                c=c-java.lang.Character.charCount(stack & 0xffffff);
+                break;
+            case COUNT4:
+                i=i+4;
+                // x 2 for surrogates
+                c=c-(java.lang.Character.charCount(stack) * 2);
+                break;
             }
         }
-        throw new IllegalStateException();
     }
 
 }
